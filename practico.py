d3199a24fba85b88f5543091da257d13e95c47c8
Milanesa21 <diegojara88574@gmail.com>
2024-04-18 16:12:05 -0300

    Se mejoro la logica de deteccion de errores en caso de vacio o letras

--- practico.py
+++ practico.py
@@ -1,34 +1,38 @@
 from pandas import pandas as pd
 
-
 # Carga de datos
 data_frame = pd.read_csv('edades-30Alumnos2.csv')
 
-#Funcion de analisis matematico
+# Función de análisis matemático
 def analisis_matematico(data_frame):
     
-    #Verifica si el data_frame esta vacio
-    if data_frame.empty:
-        print("El data_frame esta vacio, no se pueden realizar analisis")
+    # Verifica si las columnas necesarias existen en el data_frame
+    required_columns = ["fi"]
+    if not all(column in data_frame.columns for column in required_columns):
+        print("El data_frame no tiene las columnas necesarias para realizar el análisis")
         return
     
-    #Verifica si las columnas necesarias existen en el data_frame
-    requires_columns = ["fi"]
-    if not all (column in data_frame.columns for column in requires_columns):
-        print("El data_frame no tiene las columnas necesarias para realizar el analisis")
+    # Verifica que la columna "fi" contenga solo valores numéricos
+    if not data_frame["fi"].apply(lambda x: isinstance(x, (int))).all():
+        print("La columna 'fi' debe contener solo valores numéricos.")
         return
     
-    #Realiza los calculos si pasa las validaciones
+    #Verifica que los datos sean 
+    if data_frame['fi'].isna().any():
+        print("La columna 'fi' no puede contener valores nulos.")
+        return
+    
+    # Realiza los cálculos si pasa las validaciones
     data_frame["Fi"] = data_frame["fi"].cumsum()
     data_frame["ri"] = data_frame["fi"] / data_frame["fi"].sum()
-    data_frame["Ri"] = (data_frame["ri"].cumsum())
+    data_frame["Ri"] = data_frame["ri"].cumsum()
     data_frame["pi%"] = data_frame["ri"] * 100
-    data_frame["Pi%"] = (data_frame["pi%"].cumsum())
+    data_frame["Pi%"] = data_frame["pi%"].cumsum()
     
-    # Print de los calculos hechos
+    # Print de los cálculos hechos
     print(data_frame)
-    #Copia al portapapeles los calculos
+    # Copia al portapapeles los cálculos
     data_frame.to_clipboard()
 
-#Llamada a la funcion
+# Llamada a la función
 analisis_matematico(data_frame)
